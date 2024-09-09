9b7ea9e6271e713b4e92b82eb05a0cce51dd7172
ajitdhdharia <ajitdhdhariaaa@gmail.com>
2024-09-09 15:00:30 -0400

    implement initial file and folder structure

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-ibm-plex-serif",
+});
+
+export const metadata: Metadata = {
+  title: "FinFlow Bank",
+  description: "FinFlow is a modern banking platform for everyone.",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${ibmPlexSerif.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
