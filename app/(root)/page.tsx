a0d06b6f74ef3acb12b5095c78c807b0df8b971a
ajitdhdharia <ajitdhdhariaaa@gmail.com>
2024-09-09 23:29:52 -0400

    feat: implement menu sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
-const page = () => {
+const Home = () => {
+  let loggedIn = { firstName: "Ajit" };
   return (
-    <div>page</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            subtext="Access and Manage your account and transactions efficiently"
+            user={loggedIn?.firstName || "Guest"}
+          />
 
-export default page
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
